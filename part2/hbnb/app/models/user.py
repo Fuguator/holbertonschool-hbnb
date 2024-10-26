0de2aa01544f4950937d6c4a5dcf19fb990ad64e
Fuguator <abbsvfuad@gmail.com>
2024-10-26 23:53:05 +0400

    Added validation

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -4,7 +4,7 @@ from app.models.__init__ import BaseModel
 
 
 class User(BaseModel):
-    def __init__(self, first_name, last_name, email, is_admin=False) -> None:
+    def __init__(self, first_name, last_name: str, email: str, is_admin=False) -> None:
         self.id = str(uuid.uuid4())
         self.first_name = first_name
         self.last_name = last_name
@@ -23,3 +23,14 @@ class User(BaseModel):
             if hasattr(self, key):
                 setattr(self, key, value)
         self.save()
+        
+    def validation(self, first_name, last_name):
+        if len(self.first_name) > 50:
+            raise ValueError("first_name can't exceed 50 characters")
+        elif len(self.last_name) > 50:
+            raise ValueError("last_name can't exceed 50 characters")
+        elif not isinstance(self.is_admin, bool):
+            raise ValueError("You are not admin")
+        self.first_name = first_name
+        self.last_name - last_name
+        self.is_admin = True
