e32567385d3922256c640ba75339af530a016aca
Fuguator <abbsvfuad@gmail.com>
2024-10-27 11:02:17 +0400

    Added validation

--- part2/hbnb/app/models/place.py
+++ part2/hbnb/app/models/place.py
@@ -5,6 +5,7 @@ from app.models.__init__ import BaseModel
 
 class Place(BaseModel):
     def __init__(self, title: str, description: str, price: float, latitude: str, longitude: str, owner) -> None:
+        super().__init__()
         self.id = str(uuid.uuid4())
         self.title = title
         self.description = description
@@ -12,8 +13,6 @@ class Place(BaseModel):
         self.latitude = latitude
         self.longitude = longitude
         self.owner = owner
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
     
     def save(self):
         """Update the updated_at timestamp whenever the object is modified"""
@@ -25,3 +24,27 @@ class Place(BaseModel):
             if hasattr(self, key):
                 setattr(self, key, value)
         self.save()
+
+    def title_validation(self, text):
+        if len(text) < 1:
+            raise ValueError("title's length is empty")
+        elif len(text) > 100:
+            raise ValueError("title's length is over 100 characters")
+        self.title = text
+
+    def price_validation(self, value):
+        if value < 0:
+            raise ValueError("Price can't be negative")
+        self.price = value
+
+    def latitude_validation(self, value):
+        if len(value) not in range(-90, 90):
+            raise ValueError("latitude must be in range -90 : 90")
+        self.latitude = value
+        
+    def longitude_validation(self, value):
+        if len(value) not in range(-180, 180):
+            raise ValueError("longtitude must be in range -180 : 180")
+        self.longitude = value
+    
+    
