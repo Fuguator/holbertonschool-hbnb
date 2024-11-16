b225180d2f136eb84d27b99b9a6cced8c8994572
Fuguator <abbsvfuad@gmail.com>
2024-11-16 22:59:27 +0400

    add: jwt initialization

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
+from app.api.v1.auth import api as auth_ns
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 
@@ -18,4 +19,5 @@ def create_app(config_class="config.DevelopmentConfig"):
     jwt.init_app(app)
 
     api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
     return app
